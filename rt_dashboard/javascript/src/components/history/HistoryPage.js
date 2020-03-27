1555b417840d00fcaae3fe2ed56b92f7b50fd130
Geno Roupsky <geno@roupsky.name>
2020-03-27 13:16:59 +0200

    Load data async

--- rt_dashboard/javascript/src/components/history/HistoryPage.js
+++ rt_dashboard/javascript/src/components/history/HistoryPage.js
@@ -1,5 +1,6 @@
 import templateHtml from './HistoryPage.html'
 import {loadTemplate} from "../../utils/dom";
+import {getHistory} from "../../api";
 
 export default class HistoryPage extends HTMLElement {
   constructor() {
@@ -9,10 +10,25 @@ export default class HistoryPage extends HTMLElement {
 
     loadTemplate(this.shadowRoot, templateHtml)
 
-    const chart = this.shadowRoot.getElementById('chart')
-    this.shadowRoot.getElementById('zoom-in').addEventListener('click', () => chart.zoomIn())
-    this.shadowRoot.getElementById('zoom-out').addEventListener('click', () => chart.zoomOut())
-    this.shadowRoot.getElementById('pan-left').addEventListener('click', () => chart.panLeft())
-    this.shadowRoot.getElementById('pan-right').addEventListener('click', () => chart.panRight())
+    this._chart = this.shadowRoot.getElementById('chart')
+
+    this._controller = new AbortController()
+
+    this.shadowRoot.getElementById('zoom-in').addEventListener('click', () => this._chart.zoomIn())
+    this.shadowRoot.getElementById('zoom-out').addEventListener('click', () => this._chart.zoomOut())
+    this.shadowRoot.getElementById('pan-left').addEventListener('click', () => this._chart.panLeft())
+    this.shadowRoot.getElementById('pan-right').addEventListener('click', () => this._chart.panRight())
+  }
+
+  async connectedCallback () {
+    const response = await getHistory({signal: this._controller.signal})
+    if (response.ok) {
+      this._chart.setHistoryData(await response.json())
+    }
+  }
+
+  disconnectedCallback () {
+    // cancel the pending request if any
+    this._controller.abort()
   }
 }

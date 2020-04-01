396003182372b2443ea3429dd79bd281ca20849f
Dimitar Nikolov <dimitar@djangsters.de>
2020-04-02 00:22:54 +0300

    fix linter issues??

--- webpack.devserver.config.js
+++ webpack.devserver.config.js
@@ -5,8 +5,8 @@ module.exports = {
         queues: [
           { name: '[test]', count: 0, url: '/admin/rt_dashboard/inner/test' },
           { name: '[running]', count: 1, url: '/admin/rt_dashboard/inner/running' },
-          { name: 'default', count: 0, url: '/admin/rt_dashboard/inner/default' }
-        ]
+          { name: 'default', count: 0, url: '/admin/rt_dashboard/inner/default' },
+        ],
       })
     })
     app.get('/admin/rt_dashboard/inner/workers.json', (req, res) => {
@@ -14,12 +14,12 @@ module.exports = {
         workers: [{
           state: 'pause',
           name: 'v-yf.2193',
-          queues: ['default', 'low_prio_queue']
+          queues: ['default', 'low_prio_queue'],
         }, {
           state: 'busy',
           name: 'v-yf.2192',
-          queues: ['empty', 'high_prio_queue']
-        }]
+          queues: ['empty', 'high_prio_queue'],
+        }],
       })
     })
 
@@ -32,7 +32,7 @@ module.exports = {
       error_message: error,
       enqueued_at: new Date().getTime(),
       started_at: new Date().getTime(),
-      ended_at: new Date().getTime()
+      ended_at: new Date().getTime(),
     })
     const getJobsHandler = (pagination) => (req, res) => {
       res.json({
@@ -40,31 +40,31 @@ module.exports = {
           generateTask('fa02bf65-0527-40d0-acae-9f3c041fAAAA', 'General error', 'failed'),
           generateTask('fa02bf65-0527-40d0-acae-9f3c041f3EEE', null, 'finished'),
           generateTask('fa02bf65-0527-40d0-acae-9f3c041f3FFF', null, 'finished'),
-          generateTask('fa02bf65-0527-40d0-BBBB-9f3c041fBBBB', null, 'running')
+          generateTask('fa02bf65-0527-40d0-BBBB-9f3c041fBBBB', null, 'running'),
         ],
-        pagination
+        pagination,
       })
     }
     app.get('/admin/rt_dashboard/inner/jobs/%5Btest%5D/1.json',
       getJobsHandler({
         pages_in_window: [{ number: 1 }, { number: 2 }],
-        next_page: { url: '/admin/rt_dashboard/inner/%5Btest%5D/5' }
+        next_page: { url: '/admin/rt_dashboard/inner/%5Btest%5D/5' },
       }))
     app.get('/admin/rt_dashboard/inner/jobs/%5Btest%5D/2.json',
       getJobsHandler({
         pages_in_window: [{ number: 1 }, { number: 2 }],
-        prev_page: { url: '/admin/rt_dashboard/inner/%5Btest%5D/1' }
+        prev_page: { url: '/admin/rt_dashboard/inner/%5Btest%5D/1' },
       }))
     app.get('/admin/rt_dashboard/inner/jobs/%5Brunning%5D/1.json',
       getJobsHandler({
         pages_in_window: [{ number: 1 }, { number: 2 }],
-        next_page: { url: '/admin/rt_dashboard/inner/%5Btest%5D/5' }
+        next_page: { url: '/admin/rt_dashboard/inner/%5Btest%5D/5' },
       }))
     app.get('/admin/rt_dashboard/inner/jobs/default/1.json', (req, res) => {
       res.json({
         jobs: [
         ],
-        pagination: { pages_in_window: 0, next_page: null, prev_page: null }
+        pagination: { pages_in_window: 0, next_page: null, prev_page: null },
       })
     })
     app.post('/admin/rt_dashboard/inner/queue/default/delete', (req, res) => {
@@ -76,5 +76,5 @@ module.exports = {
     app.post('/admin/rt_dashboard/inner/job/fa02bf65-0527-40d0-BBBB-9f3c041fBBBB/cancel', (req, res) => {
       res.json({})
     })
-  }
+  },
 }

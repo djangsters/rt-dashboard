2e9041e994062e5b9385bb7c4d810ba9686e72b0
Iwan Trofimtschuk <iwan@djangsters.de>
2020-05-25 12:26:20 +0200

    align python versions with travis

--- setup.py
+++ setup.py
@@ -30,5 +30,8 @@ setup(
         # 'Development Status :: 7 - Inactive',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )

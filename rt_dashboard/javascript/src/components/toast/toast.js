42d824a8de8a4383c54a6233127839304888bcbc
Dimitar Nikolov <dimitar@djangsters.de>
2020-05-29 10:10:36 +0300

    refactor implementation

--- rt_dashboard/javascript/src/components/toast/toast.js
+++ rt_dashboard/javascript/src/components/toast/toast.js
@@ -9,45 +9,56 @@ export default class Toast extends HTMLElement {
 
     loadTemplate(this.attachShadow({ mode: 'open' }), html)
 
+    this.onToast = this.onToast.bind(this)
     this.showToast = this.showToast.bind(this)
+    this.hideToast = this.hideToast.bind(this)
     this.onClose = this.onClose.bind(this)
   }
 
   connectedCallback () {
-    document.addEventListener('toast', this.showToast)
+    this.toastTitle = this.shadowRoot.querySelector('#title')
+    this.toastMessage = this.shadowRoot.querySelector('#message')
+    this.toastTimestamp = this.shadowRoot.querySelector('#time')
+
+    document.addEventListener('toast', this.onToast)
     this.toast = this.shadowRoot.querySelector('.toast')
-    const closeBtn = this.shadowRoot.querySelector('button.close')
-    closeBtn.addEventListener('click', this.onClose)
+    this.closeBtn = this.shadowRoot.querySelector('button.close')
+    this.closeBtn.addEventListener('click', this.onClose)
   }
 
   disconnectedCallback () {
-    document.removeEventListener('toast', this.showToast)
+    document.removeEventListener('toast', this.onToast)
+    this.closeBtn.removeEventListener('click', this.onClose)
   }
 
-  onClose (event) {
+  onClose () {
+    this.hideToast()
+  }
+
+  hideToast () {
     this.toast.classList.add('hide')
     this.toast.classList.remove('show')
   }
 
-  showToast (event) {
-    const { detail: { title, message } } = event
-    const timestamp = new Date().getTime()
-    this.toast.classList.add('hide')
-    this.toast.classList.remove('show')
-    const toastTitle = this.shadowRoot.querySelector('#title')
-    const toastMessage = this.shadowRoot.querySelector('#message')
-    const toastTimestamp = this.shadowRoot.querySelector('#time')
-    toastTitle.innerHTML = title
-    toastMessage.innerHTML = message
-    toastTimestamp.innerHTML = timestamp
+  showToast () {
     this.toast.classList.add('show')
     this.toast.classList.remove('hide')
+  }
 
-    cancelTimeout(this.timeout)
+  onToast (event) {
+    const { detail: { title, message } } = event
+    const timestamp = new Date().getTime()
+    this.hideToast()
+
+    this.toastTitle.innerHTML = title
+    this.toastMessage.innerHTML = message
+    this.toastTimestamp.innerHTML = timestamp
+
+    this.showToast()
+
+    clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
-        this.toast.classList.add('hide')
-        this.toast.classList.remove('show')
-      }
+      this.hideToast()
     }, TOAST_TIMEOUT)
   }
 }
